9b571f64e93cee3f3e91e53510b8353990b48127
rasekh moafi <rasekhmoafi@gmail.com>
2022-12-19 12:17:46 +0330

    asd

--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -3,33 +3,36 @@ import { User } from 'src/typeorm/entities/User';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
-
+import { CreateUserParams } from 'src/users/utils/types';
 
 @Injectable()
 export class UsersService {
-    constructor(
-        @InjectRepository(User) private userRepository: Repository<User>
-    ) {}
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
 
-    users: any = [
-        {username: 'rasekh', email: 'rasekh@gmail.com'},
-        {username: 'John', email: 'rasekh@gmail.com'},
-        {username: 'sali', email: 'rasekh@gmail.com'},
-        {username: 'james', email: 'rasekh@gmail.com'},
-    ]
+  users: any = [
+    { username: 'rasekh', email: 'rasekh@gmail.com' },
+    { username: 'John', email: 'rasekh@gmail.com' },
+    { username: 'sali', email: 'rasekh@gmail.com' },
+    { username: 'james', email: 'rasekh@gmail.com' },
+  ];
 
-    getUsers() {
-        return this.users
+  getUsers() {
+    return this.users;
+  }
+  getUserById(id: number) {
+    const user = this.users[id];
+    if (!user) {
+      throw new HttpException('User Not Founded', HttpStatus.NOT_FOUND);
     }
-    getUserById(id: number) {
-        const user = this.users[id]
-        if(!user) {
-            throw new HttpException("User Not Founded", HttpStatus.NOT_FOUND)
-        }
-        return user
-    }
-    createUser(user: CreateUserDto) {
-        this.users.push(user)
-        return 'created'
-    }
+    return user;
+  }
+  createUser(userDetails: CreateUserParams) {
+    const newUser = this.userRepository.create({
+      ...userDetails,
+      createdAt: new Date()
+    });
+    return this.userRepository.save(newUser)
+  }
 }
